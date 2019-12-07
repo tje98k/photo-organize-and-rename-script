ca1f2c19e5fc9aff56b600f3196d833fd6306f08
Taylor Egleston <tjegleston@gmail.com>
2019-12-07 17:50:59 -0600

    .py script
    Script so far takes dir name, renames to exif datetime taken and
    organizes into folders by date.

---
+++ photo_rename_organize.py
@@ -0,0 +1,39 @@
+
+from PIL import Image
+import os
+import time
+import shutil
+
+
+folders=[]
+print('Enter dir of photos')
+dir = input()
+os.chdir(dir)
+print('Changing dir to' + (os.getcwd()))
+
+for img in os.listdir():
+        if str((Image.open(img)._getexif()[36867]))[0:10] in img:
+           pass
+        else:
+            if '.jpg' in img:
+                os.rename(img,str(Image.open(img)._getexif()[36867]).replace(':', '-')+'.jpg')
+print('Renamed photos')
+for img in os.listdir():
+    if img[0:10] in os.listdir():
+            pass
+    else:
+            os.mkdir(img[0:10])
+for img in os.listdir():
+    if '.jpg' in img:
+        pass
+    else:
+        folders.append(img)
+print('created folder(s)' + str(folders))
+for img in os.listdir():
+    if '.jpg' in img:
+        shutil.move(img, str(img[0:10] + '/' + img))
+print('moved images to folders')
+
+
+ 
+
